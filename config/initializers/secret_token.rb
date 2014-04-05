8787c41a04ba8d14a615118c1ef99601a493a3b9
scriptZilla <david.lowery@colorado.edu>
2014-04-05 16:07:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MileHiFly::Application.config.secret_key_base = '5d55f478f885859c690fe107fe0a6306f9b5232e43b3a8e368284a952893481a07e56e5f2e582120f8a3b7b49e7b8a6d64020713178b80092b4cb7938c0d60c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MileHiFly::Application.config.session_store :cookie_store, key: '_mileHiFly_session'
