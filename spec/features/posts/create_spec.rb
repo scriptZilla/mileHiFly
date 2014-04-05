fc061f519b2bc5657f7b9c84e7f20ddafedc379c
scriptZilla <david.lowery@colorado.edu>
2014-04-05 17:05:55 -0600

    Add more tests & some basic validation for the post model

--- spec/features/posts/create_spec.rb
+++ spec/features/posts/create_spec.rb
@@ -28,6 +28,23 @@ describe "Creating posts" do
     expect(Post.count).to eq(0)
 
     visit "/posts"
-    expect(page).to_not have_contents("rainbow brown cutt throat")
+    expect(page).to_not have_content("rainbow brown cutt throat")
+  end
+
+  it "displays an error when the new post has no actual content" do
+    expect(Post.count).to eq(0)
+    visit "/posts"
+    click_link "New Post"
+    expect(page).to have_content("New post")
+
+   fill_in "Title", with: "Fly fishin'"
+    fill_in "Content", with: ""
+    click_button "Create Post"
+    
+    expect(page).to have_content("error")
+    expect(Post.count).to eq(0)
+
+    visit "/posts"
+    expect(page).to_not have_content("Fly fishin'")
   end
 end
