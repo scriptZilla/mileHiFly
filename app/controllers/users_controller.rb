84acf841c013112851d3b6e32934da4e5d154b2b
scriptZilla <david.lowery@colorado.edu>
2014-04-05 19:07:59 -0600

    Touch up some specs for the user & post models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
 
   # GET /users
   # GET /users.json
-  def index
-    @users = User.all
-  end
+  #def index
+  #  @users = User.all
+  #end
 
   # GET /users/1
   # GET /users/1.json
-  def show
-  end
+  #def show
+  #end
 
   # GET /users/new
   def new
