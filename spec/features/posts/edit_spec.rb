5b1e61a1d10bd805560243271351f42a3064c38e
scriptZilla <david.lowery@colorado.edu>
2014-04-05 18:15:47 -0600

    Slight refactoring on edit spec for post model

--- spec/features/posts/edit_spec.rb
+++ spec/features/posts/edit_spec.rb
@@ -1,23 +1,35 @@
 require "spec_helper"
 
  describe "Editing posts" do
-   it "Updates an edited post" do
-     visit "/posts"
-     post = Post.create(title: "Fly fishin'", content: "rainbow brown cut throat")     
+
+   def update_post(options={})
+     options[:title] ||= "Roll Cast"
+     options[:content] ||= "Use for tight trees"
+
+     post = options[:post]
 
      visit "/posts"
      within "#post_#{post.id}" do
        click_link "Edit"
      end
 
-     fill_in "Title", with: "Some New Title"
-     fill_in "Content", with: "Some New Content"
+     fill_in "Title", with: options[:title]
+     fill_in "Content", with: options[:content]
      click_button "Update Post"
+   end
 
+   it "Updates an edited post" do
+     post = Post.create(title: "Streamer", content: "Spring Pattern")
+     update_post post: post, 
+                 title: "Some New Title",
+                 content: "Some New Content"
      post.reload
  
      expect(page).to have_content("Post was successfully updated")
      expect(post.title).to eq("Some New Title")
      expect(post.content).to eq("Some New Content")
+     
+     expect(page).to_not have_content("Streamer")
+     expect(page).to_not have_content("Spring Pattern")
    end
 end
